b7b4f954ee040f0e0bb3dc50805ff874824746db
Juan Torres <juan852004da@gmail.com>
2024-03-06 06:25:26 -0500

    Commit diferentes tipod de datos

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 let Nada = [];
 //crear un array con elementos:
 let frutas =["Manzana","Banano","Naranja"];
+//crear un array con elementos de diferentes tipos de datos
+let DiferentesTipos = [1, 'dos', true, { nombre: 'Juan' }];
