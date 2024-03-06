f9efe8b8606625b4d744fca8270deeb2fda02c89
Juan Torres <juan852004da@gmail.com>
2024-03-06 06:30:09 -0500

    Commit de array constructor

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let Nada = [];
 let frutas =["Manzana","Banano","Naranja"];
 //crear un array con elementos de diferentes tipos de datos
 let DiferentesTipos = [1, 'dos', true, { nombre: 'Juan' }];
+//crear un array utilizando el constructor array
+let arrayConstructor = new Array(3);
